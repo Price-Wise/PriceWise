f1cd061a5ff5883b2484e4d44549561847af8d70
Emam Mounqer <emam.anagreh@gmail.com>
2023-05-28 18:05:47 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+class App:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Price Wise")
+
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    app = App()
