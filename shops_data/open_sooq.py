e01a4453c1334b8f7d8d69f1cdf2605764ac4f27
AnasAlsallak <anasalsallak@gmail.com>
2023-05-30 03:36:04 +0300

    open_sooq m done

---
+++ shops_data/open_sooq.py
@@ -0,0 +1,50 @@
+from shops_data.item import Item
+from shops_data.shop_base import ShopBase
+from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup, Tag
+from shops_data.shop_category import ShopCategory
+
+class openSooq(ShopBase):
+    STORE = "Open Sooq"
+
+    @property
+    def shop_categories(self) -> list[ShopCategory]:
+        return [ShopCategory.ALL]
+
+    def get_items(self, search_item) -> list[Item]:
+        with sync_playwright() as p:
+            browser = p.chromium.launch()
+            page = browser.new_page()
+            page.goto(
+                f"https://jo.opensooq.com/ar/find?PostSearch[term]={search_item}")
+            page.wait_for_load_state()
+            html = page.content()
+
+            soup = BeautifulSoup(html, 'html.parser')
+
+            search_items = soup.find_all('div', class_='mb-32 relative')
+            return [self.get_item_from_dev(search_item) for search_item in search_items]
+
+    def get_item_from_dev(self, search_item: Tag) -> Item:
+        title_elem = search_item.find('h2', class_='font-20 breakWord overflowHidden')
+        title = title_elem.text.strip() if title_elem else 'N/A'
+
+        price_elem = search_item.find('div', class_='postPrice ms-auto bold alignSelfCenter font-19')
+        price = price_elem.text.strip() if price_elem else 'N/A'
+
+        image_element = search_item.find('img', class_='width-100 height-100')
+        image_url = image_element.get('src', '') if isinstance(
+            image_element, Tag) else ''
+
+        link_element = search_item.find('a', class_='flex flexNoWrap p-16 blackColor radius-8 grayHoverBg ripple boxShadow2 relative')
+        link = link_element.get('href', '') if isinstance(
+            link_element, Tag) else ''
+
+        return Item(title, price, openSooq.STORE, link, image_url, '')
+
+
+if __name__ == "__main__":
+    open_sooq = openSooq()
+    data = open_sooq.get_items("خزانة")
+    print(open_sooq.shop_categories)
+    print(data)
---
+++ shops_data/tests/test_open_sooq.py
@@ -0,0 +1,28 @@
+import pytest 
+# from open_sooq import openSooq
+# from bs4 import BeautifulSoup
+from item import Item
+
+
+def test_get_items_happy_path(self, open_sooq):
+    search_item = "خزانة"
+    items = open_sooq.get_items(search_item)
+    assert isinstance(items, list)
+    assert all(isinstance(item, Item) for item in items)
+
+def test_get_items_expected_failure(self, open_sooq):
+    search_item = "nonexistent_item"
+    items = open_sooq.get_items(search_item)
+    assert isinstance(items, list)
+    assert len(items) == 0
+
+# def test_get_item_from_dev_edge_case(self, open_sooq):
+#     search_item = BeautifulSoup('', 'html.parser').new_tag('div')
+#     item = open_sooq.get_item_from_dev(search_item)
+#     assert isinstance(item, Item)
+#     assert item.title == 'N/A'
+#     assert item.price == 'N/A'
+#     assert item.store == open_sooq.STORE
+#     assert item.link == ''
+#     assert item.image_url == ''
+#     assert item.additional_info == ''
