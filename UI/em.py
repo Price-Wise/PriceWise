580e7d86ed669c408721048d874201af9ad64cbe
Emam Mounqer <emam.anagreh@gmail.com>
2023-06-01 22:31:03 +0000

    add on history click

--- UI/em.py
+++ UI/em.py
@@ -4,6 +4,7 @@ from typing import Callable
 
 class UI_V1:
     on_search_listener: list[Callable] = []
+    on_history_click_listener: list[Callable] = []
 
     @staticmethod
     def init():
@@ -18,10 +19,20 @@ class UI_V1:
         UI_V1.on_search_listener.append(listener)
 
     @staticmethod
+    def add_on_history_click_listener(listener):
+        UI_V1.on_history_click_listener.append(listener)
+
+    @staticmethod
     @eel.expose
     def on_search(query):
         for listener in UI_V1.on_search_listener:
             listener(query)
+
+    @staticmethod
+    @eel.expose
+    def on_history_click(id):
+        for listener in UI_V1.on_history_click_listener:
+            listener(id)
 
 
 if __name__ == '__main__':
