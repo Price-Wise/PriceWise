54571bebdaa9c47bc3a3514873c168ad5d2971ca
Mohammad-Shahin23 <mohammad.shahin2221@gmail.com>
2023-06-02 13:12:27 +0000

    added event lesteners for bothe the enter key and onclick button for the search bar

--- UI/search_bar.py
+++ UI/search_bar.py
@@ -1,4 +1,7 @@
 import eel
+import asyncio
+from shops_data.DNA import DNA
+
 
 # Set web files folder and optionally specify which file types to check for eel.expose()
 eel.init('UI')
@@ -9,6 +12,15 @@ def perform_search(query):
     # Here, you can implement your search logic using the provided query
     # For simplicity, let's just print the query for now
     print(f"Performing search: {query}")
+    print(search_DNA(query))
+
+    
+def search_DNA(query):
+    # Here, you can implement your search logic using the provided query and return the data to the UI
+    dna = DNA()
+    data = asyncio.run(dna.get_items(query))
+    return data
+
 
 # Start the application
 eel.start('web/search_bar/search_bar.html', size=(400, 400))
