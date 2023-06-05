1150101adee213b5e4570fa34184cb1a87c067d8
saifobe <obeidatsaif93@gmail.com>
2023-06-05 14:45:13 +0000

    icons has been added

--- shops_data/search_logic.py
+++ shops_data/search_logic.py
@@ -18,7 +18,7 @@ from shops_data.smartbuy import Smartbuy
 
 class SearchLogic:
     shops: list[ShopBase] = [Amazon(), ammancart(), DNA(),
-                             Ebay(), Matjarii(), openSooq(),  Smartbuy(), ]
+                             Ebay(), Matjarii(), openSooq(),  Smartbuy() ]
 
     @staticmethod
     async def search(search_item: str, search_options: Optional[SearchOptions] = None) -> list[Item]:
