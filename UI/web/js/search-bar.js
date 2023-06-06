0172e7163dc7ecfb9dad123e04f66378c70fa09f
Emam Mounqer <emam.anagreh@gmail.com>
2023-06-06 08:22:57 +0000

    fix the search options

--- UI/web/js/search-bar.js
+++ UI/web/js/search-bar.js
@@ -12,14 +12,6 @@ function performSearch() {
     });
 }
 
-// function performSearch() {
-//     console.log(getFormValues());
-//     const query = document.getElementById("search-input").value;
-//     const searchOptions = getFormValues();
-//     eel.on_search(query, searchOptions);
-//     console.log("Search performed");
-// }
-
 function showLoadingAnimation() {
     var loadingElement = document.getElementById("loading-animation");
     loadingElement.style.display = "flex";
@@ -52,25 +44,60 @@ input.addEventListener("keydown", function (event) {
 //#region
 const formOptions = document.getElementById("search-options");
 
-const checkAllCheckboxOption = document.getElementById("check-all-stores");
-const checkboxesOptions = document.querySelectorAll('input[name="stores"]');
-
-console.log(checkboxesOptions);
+const storesSearchOptions = document.getElementById("stores-search-options");
+let checkAllCheckboxOption = document.getElementById("check-all-stores");
+const storesExpandBtn = document.getElementById("stores-search-options-expand-btn");
+const storeLocationDd = document.getElementById("stores-location-dd");
+const categoryDd = document.getElementById("category-dd");
+const checkboxesOptions = [];
+console.log(storeLocationDd);
+updateStoresSearchOptions(shopsInfo);
 
 // Events
-checkAllCheckboxOption.addEventListener("change", function () {
-    checkboxesOptions.forEach(function (checkbox) {
-        checkbox.checked = checkAllCheckboxOption.checked;
-    });
-});
-checkboxesOptions.forEach(function (checkbox) {
-    checkbox.addEventListener("change", function () {
-        if (!this.checked) {
-            checkAllCheckboxOption.checked = false;
-        }
-    });
-});
+storesExpandBtn.addEventListener("click", filterStores);
+storeLocationDd.addEventListener("change", filterStores);
+categoryDd.addEventListener("change", filterStores);
+//
 
+function updateStoresSearchOptions(newStores) {
+    storesSearchOptions.innerHTML = `
+    <div class="form-check store check list" style="font-size: small">
+        <input
+            class="form-check-input"
+            type="checkbox"
+            id="check-all-stores"
+            value="ALL"
+        /><label class="form-check-label" for="check-all-stores"
+            >All</label
+        >
+    </div>`;
+    checkAllCheckboxOption = document.getElementById("check-all-stores");
+    checkAllCheckboxOption.addEventListener("change", checkAllBtnHandler);
+
+    for (const store of newStores) {
+        const container = document.createElement("div");
+        container.classList.add("form-check", "store", "check", "list");
+        container.style.fontSize = "small";
+
+        const input = document.createElement("input");
+        container.appendChild(input);
+        input.classList.add("form-check-input");
+        input.type = "checkbox";
+        input.id = "formCheck-" + store.name;
+        input.value = store.name;
+        input.name = "stores";
+
+        const label = document.createElement("label");
+        container.appendChild(label);
+        label.classList.add("form-check-label");
+        label.htmlFor = "formCheck-" + store.name;
+        label.innerText = store.name;
+
+        input.addEventListener("change", checkAllStoresHandler);
+        checkboxesOptions.push(input);
+        storesSearchOptions.appendChild(container);
+    }
+}
 // functions
 
 function getFormValues() {
@@ -79,7 +106,7 @@ function getFormValues() {
     for (let i = 0; i < inputs.length; i++) {
         formValues[inputs[i].name] = inputs[i].value;
     }
-    const checkboxesStores = formOptions.elements["stores"];
+    const checkboxesStores = formOptions.elements["stores"] || [];
     const stores = [];
 
     for (let i = 0; i < checkboxesStores.length; i++) {
@@ -89,5 +116,35 @@ function getFormValues() {
     }
     formValues["stores"] = stores;
     return formValues;
+}
+
+function filterStores() {
+    let stores = shopsInfo;
+    const formValues = getFormValues();
+    console.log(formValues);
+
+    if (formValues["category"] && formValues["category"] !== "All")
+        stores = stores.filter(
+            (store) =>
+                store.categories.includes(formValues["category"]) ||
+                store.categories.includes("GENERAL")
+        );
+
+    if (formValues["stores_location"] && formValues["stores_location"] !== "All")
+        stores = stores.filter((store) => store.stores_location === formValues["stores_location"]);
+
+    updateStoresSearchOptions(stores);
+}
+
+function checkAllStoresHandler() {
+    if (!this.checked) {
+        checkAllCheckboxOption.checked = false;
+    }
+}
+
+function checkAllBtnHandler() {
+    checkboxesOptions.forEach(function (checkbox) {
+        checkbox.checked = checkAllCheckboxOption.checked;
+    });
 }
 //#endregion
