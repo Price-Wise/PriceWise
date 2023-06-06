da005b121814c9321d4a541349efe4fb906bcc39
Mohammad-Shahin23 <mohammad.shahin2221@gmail.com>
2023-06-06 12:11:02 +0000

    fixed design

--- UI/web/js/search-card.js
+++ UI/web/js/search-card.js
@@ -36,7 +36,7 @@ const cardTemplate = `
     <div class="d-flex flex-row justify-content-center align-items-center"
       style="border-radius: 39px;width: 60px;height: 60px;position: absolute;bottom: -31px;right: 10px;">
       <button class="btn btn-primary card-iconBtn" data-bss-hover-animate="pulse" type="button"
-        style="border-style: none;background: #b86868;width: 60px;height: 60px;border-radius: 39px;box-shadow: 2px 2px 20px -1px #757575;"
+        style="border-style: none;background: #42a2f1;width: 60px;height: 60px;border-radius: 39px;box-shadow: 2px 2px 20px -1px #757575;"
         data-bs-toggle="modal" data-bs-target="#modal_image"><i class="fa-sharp fa-solid fa-shop"
           style="color: rgb(255,255,255);font-size: 20px;"></i></button>
       <div class="modal fade" role="dialog" tabindex="-1" id="modal_image-5"
@@ -95,7 +95,7 @@ const cardTemplate = `
 // Create a function to generate the card from the template
 
 const amazonIcon = `<i class="fab fa-amazon" style="color: rgb(255,255,255);font-size: 20px;"></i>`;
-const ebayIcon = `<i class="fa-brands fa-ebay" style="color: rgb(255,255,255);font-size: 20px;"></i>`;
+const ebayIcon = `<i class="fa-brands fa-ebay" style="color: rgb(255,255,255);font-size: 30px;"></i>`;
 const aliBabaIcon = `<svg style="color: rgb(255,255,255);font-size: 20px;" fill="#FF6600" xmlns="http://www.w3.org/2000/svg" stroke="#FF6600"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M14.391 16.22c-.963.044-.865-.459-.302-1.234 1.32-1.768 3.82-4.236 3.906-5.982.151-2.283-2.143-3.026-4.501-3.004-1.645.022-3.344.492-4.501.906C5 8.315 2.489 10.576.909 13.076-.768 15.554-.216 17.923 3.322 18c2.716-.109 4.48-.862 6.32-1.802.01 0-5.086 1.453-6.958.383l-.008-.002c-.193-.11-.404-.264-.457-.683-.012-.885 1.46-1.802 2.283-2.097v-1.533a5.374 5.374 0 0 0 1.955.366 5.378 5.378 0 0 0 3.472-1.265c.037.13.056.278.044.447h.371c.048-.394-.172-.706-.172-.706-.333-.529-.915-.52-.915-.52s.315.137.529.466a4.953 4.953 0 0 1-4.665.932l1.21-1.2-.336-.874c2.435-.852 4.48-1.507 7.812-2.085l-.746-.624.389-.24c2.01.568 3.325.985 3.253 2.051a2.672 2.672 0 0 1-.202.611c-.584 1.158-2.326 3.09-3.029 3.898-.465.535-.92 1.06-1.245 1.562-.335.503-.54.971-.551 1.42.043 3.504 10.334-1.64 12.324-3.003-2.943 1.266-6.113 2.489-9.609 2.718z"></path></g></svg>`;
 const matjariiIcon =
     '<img src="./js/logos/OpenSooq-Logo.png" style="width: 30px; height: 30px; margin-left: 5px; margin-top: 5px; margin-bottom: 5px; margin-right: 5px;"/>';
@@ -106,6 +106,8 @@ const smartBuyIcon =
     '<img src="./js/logos/smartbuy.png" style="width: 40px; height: 50px; margin-bottom: 5px; margin-right: 5px;"/>';
 const ammanCartIcon =
     '<img src="./js/logos/favicon.webp" style="width: 30px; height: 30px; margin-left: 5px; margin-top: 5px; margin-bottom: 5px; margin-right: 5px;"/>';
+const sheInIcon =
+    '<img src="./js/logos/shein-logo-0.png" style="width: 40px; height: 50px"/>';
 
 function generateCard(item) {
     // Create a container element
@@ -139,20 +141,30 @@ function generateCard(item) {
 
     if (item.store.toLowerCase() === "amazon") {
         iconBtn.innerHTML = amazonIcon;
-    } else if (item.store.toLowerCase() === "ebay") {
+    } 
+    else if (item.store.toLowerCase() === "ebay") {
         iconBtn.innerHTML = ebayIcon;
-    } else if (item.store.toLowerCase() === "alibaba") {
+    } 
+    else if (item.store.toLowerCase() === "alibaba") {
         iconBtn.innerHTML = aliBabaIcon;
-    } else if (item.store.toLowerCase() === "matjarii") {
+    } 
+    else if (item.store.toLowerCase() === "matjarii") {
         iconBtn.innerHTML = matjariiIcon;
-    } else if (item.store.toLowerCase() === "open sooq") {
+    } 
+    else if (item.store.toLowerCase() === "open sooq") {
         iconBtn.innerHTML = openSoqeIcon;
-    } else if (item.store.toLowerCase() === "dna") {
+    } 
+    else if (item.store.toLowerCase() === "dna") {
         iconBtn.innerHTML = dnaIcon;
-    } else if (item.store.toLowerCase() === "ammancart") {
+    } 
+    else if (item.store.toLowerCase() === "ammancart") {
         iconBtn.innerHTML = ammanCartIcon;
-    } else if (item.store.toLowerCase() === "smartbuy") {
+    } 
+    else if (item.store.toLowerCase() === "smartbuy") {
         iconBtn.innerHTML = smartBuyIcon;
+    }
+    else if (item.store.toLowerCase() === "shein") {
+        iconBtn.innerHTML = sheInIcon;
     }
 
     // Add a link to the card view details
